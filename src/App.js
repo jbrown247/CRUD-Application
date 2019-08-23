916161a42fb1985b76e87697bf215f6fde7b89d5
Jeff Brown <ffsale24@gmail.com>
2019-08-22 19:53:35 -0700

    Async Await Data Post to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import "./App.css";
 
 class App extends Component {
   state = {
     posts: []
+  };
+
+  async componentDidMount() {
+    // pending > resolved (success) OR rejected (failure)
+    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    this.setState({ posts })
   };
 
   handleAdd = () => {
