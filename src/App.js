5d785da40fcf435bb37607171d093a29111adfe1
Jeff Brown <ffsale24@gmail.com>
2019-08-26 23:42:32 -0700

    Delete Button Functionality Added into CRUD app

--- src/App.js
+++ src/App.js
@@ -35,8 +35,11 @@ class App extends Component {
   };
   // console.log("Update", post);
 
-  handleDelete = post => {
-    console.log("Delete", post);
+  handleDelete = async post => {
+    await axios.delete(apiEndpoint + '/' + post.id)
+
+    const posts = this.state.posts.filter(p => p.id !== post.id);
+    this.setState({ posts });
   };
 
   render() {
