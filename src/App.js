d6b55f00fb267da35b8e1db7709182005a559317
Jeff Brown <ffsale24@gmail.com>
2019-08-28 20:37:02 -0700

    Delete Button Error Testing on Server w/ Try Catch Block Optimistic Updating

--- src/App.js
+++ src/App.js
@@ -36,10 +36,20 @@ class App extends Component {
   // console.log("Update", post);
 
   handleDelete = async post => {
+    const originalPosts = this.state.posts;
+
     const posts = this.state.posts.filter(p => p.id !== post.id);
     this.setState({ posts });
 
-    await axios.delete(apiEndpoint + '/' + post.id);
+    try {
+      await axios.delete(apiEndpoint + '/' + post.id);
+      throw new Error("");
+    }
+    catch (ex) {
+      alert('Something failed while deleting a post!');
+      this.setState({ posts: originalPosts });
+    }
+
   };
 
   render() {
